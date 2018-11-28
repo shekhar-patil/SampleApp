c36e4f19199b44f68abc7c8690d00f3620f4a89b
Your Name <you@example.com>
2018-11-28 20:58:23 +0530

    sign-up finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params)
   	if @user.save
-  		#successfully saved.
+  		flash[:success] = "Welcome to my sample application!!!"
+  		redirect_to @user
   	else
   		render 'new'
   	end
