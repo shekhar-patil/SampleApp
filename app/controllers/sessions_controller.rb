5762b224ce98363b80122e4778806ecb3a9f5315
shekhar-patil <patilshekhar900@gmail.com>
2019-01-03 14:08:26 +0530

    Implementation of Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		log_in user 
-      remember user
+      log_in user 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
 
   	else
