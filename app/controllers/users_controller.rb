7b91780f265429545baf466289706a42f6ad7345
shekhar-patil <patilshekhar900@gmail.com>
2019-01-04 13:15:46 +0530

    update user done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,41 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
+	def new
+		@user = User.new
+	end
 
-  def show
-  	@user = User.find(params[:id])
-  end
+	def show
+		@user = User.find(params[:id])
+	end
 
-  def create 
-  	@user = User.new(user_params)
-  	if @user.save
-      log_in @user
-  		flash[:success] = "Welcome to my sample application!!!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
-  end
+	def create 
+		@user = User.new(user_params)
+		if @user.save
+		log_in @user
+			flash[:success] = "Welcome to my sample application!!!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 
-  private
-  
-	  def user_params
-	  	params.require(:user).permit(:name , :email, :password , :password_confirmation)
-	  end
+	def edit 
+			@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			#handle later
+		else
+			render 'edit'
+		end
+	end
+
+
+	private
+	
+	def user_params
+		params.require(:user).permit(:name , :email, :password , :password_confirmation)
+	end
 
 end
