2176435d5355a6658ac54624a4147b1dfb367aa0
shekhar-patil <patilshekhar900@gmail.com>
2019-01-08 14:51:29 +0530

    'authenticated' function is generalized and done with test cases

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,15 +3,29 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "invalid signup information" do
 	    get signup_path
-	    assert_difference 'User.count' ,1 do
-	    post users_path, params: { user: { name:  "shekhar patil",
-	                                         email: "patilshekar900@gmail.com",
-	                                         password:              "shekhar123",
+	    assert_no_difference 'User.count' do
+	    post users_path, params: { user: { name:  "patil",
+	                                         email: "patilshekar900",
+	                                         password:              "she123",
 	                                         password_confirmation: "shekhar123" } }
 	    end
-	    follow_redirect!
-	    assert_template 'users/show'
-	    assert is_logged_in?
+			assert_template 'users/new'
+			assert_select 'div#error_explanation'
+			assert_select 'div.field_with_errors'
 
-  	end
+		end
+		
+
+		test "valid signup information" do
+			get signup_path
+			assert_difference 'User.count', 1 do
+			post users_path, params: { user: { name:  "shekhar patil",
+							email: "patilshekar900@gmail.com",
+							password:              "shekhar123",
+							password_confirmation: "shekhar123" } }
+			end
+			follow_redirect!
+			# assert_template 'users/show'
+			# assert is_logged_in?
+		end
 end
