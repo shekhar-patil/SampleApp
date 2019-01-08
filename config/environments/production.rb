acf13223b8804e49e5a700198f51413e17131599
shekhar-patil <patilshekhar900@gmail.com>
2019-01-08 19:22:09 +0530

    production.rb file updated for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['shekhar95'],
+    :password       => ENV['SS9922126108pp'],
+    :domain         => 'http://sampleapptutorial777.herokuapp.com',
     :enable_starttls_auto => true
   }
 
