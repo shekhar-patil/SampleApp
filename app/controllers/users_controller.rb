fa9464ccae3dbc4bc5f7554f4ad2462a9bad8514
shekhar-patil <patilshekhar900@gmail.com>
2019-01-09 16:51:16 +0530

    Mail can be send from localhost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	before_action :admin_user,     only: :destroy
 
 	def index
-		@users = User.where(activated: true).paginate(page: params[:page])	
+		@users = User.where(activated: false).paginate(page: params[:page])	
 	end
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
 
   config.action_mailer.perform_caching = false
 
@@ -46,9 +46,18 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'                     # Local server
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: 'patilshekhar900@gmail.com',
+    password: 'GS9922126108pl',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Use this if developing on localhost.
   # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
