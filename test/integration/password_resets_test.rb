f5c5afe6192ea5a85bf38870abc168374ae7e64d
shekhar-patil <patilshekhar900@gmail.com>
2019-01-15 00:19:48 +0530

    password reset Controller updated and test cases written

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -12,7 +12,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
   end
 
   test "password resets" do
-    
+
     get new_password_reset_path
     assert_template 'password_resets/new'
     # Invalid email
@@ -23,7 +23,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     post password_resets_path,
          params: { password_reset: { email: @user.email } }
     assert_not_equal @user.reset_digest, @user.reload.reset_digest
-    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_equal 0, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirected_to root_url
     # Password reset form
@@ -63,5 +63,20 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
+  end
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path,
+         params: { password_reset: { email: @user.email } }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobar",
+                            password_confirmation: "foobar" } }
+    assert_response :redirect
+    follow_redirect!
+    #assert_match /FILL_IN/i, response.body
   end
 end
