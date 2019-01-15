1fd1f1fdefe4c72646cdf8d83b0be53fabec204e
shekhar-patil <patilshekhar900@gmail.com>
2019-01-15 14:17:38 +0530

    Association of microposts and user is done

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,7 +56,7 @@ class UserTest < ActiveSupport::TestCase
 	    @user.password = @user.password_confirmation = " " * 6
 	    assert_not @user.valid?
 	end
-
+    
 	test "password should have a minimum length" do
 	    @user.password = @user.password_confirmation = "a" * 5
 	    assert_not @user.valid?
@@ -64,6 +64,14 @@ class UserTest < ActiveSupport::TestCase
 
 	test "authenticated? should return false for a user with nil digest" do
 	    assert_not @user.authenticated?(:remember , '')
+	end
+
+	test "associated microposts should be destroyed" do
+		@user.save
+		@user.microposts.create!(content: "Lorem ipsum")
+		assert_difference 'Micropost.count', -1 do
+			@user.destroy
+		end
 	end
 
 end
